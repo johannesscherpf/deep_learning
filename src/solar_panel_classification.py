70d56a3b2b54c14acc32c102935c50e7728c078b
hannes <johannespetersamuel.scherpf@student.hs-anhalt.de>
2025-10-14 15:29:00 +0200

    modell verbessern

--- src/solar_panel_classification.py
+++ src/solar_panel_classification.py
@@ -10,6 +10,7 @@ data = pd.read_csv('../data/train.csv')
 data = data.dropna()
 
 # Daten vorverarbeiten
+data['t']=data['t']-1
 data_X = data.drop(columns = ['t'])
 data_X = (data_X - data_X.mean()) / data_X.std()
 data_X = torch.from_numpy(data_X.values).float()
@@ -24,10 +25,15 @@ class Net(nn.Module):
         super(Net, self).__init__()
         self.fc1 = nn.Linear(60, 128)
         self.relu = nn.ReLU()
-        self.fc2 = nn.Linear(128, 9)
+        self.fc2 = nn.Linear(128, 32)
+        self.relu2 = nn.ReLU()
+        self.fc3 = nn.Linear(32, 9)
+
 
     def forward(self, x):
         x = self.relu(self.fc1(x))
+        x = self.relu2(self.fc2(x))
+        x = self.relu(self.fc3(x))
         return x
 
 # Training
@@ -41,19 +47,25 @@ batch_size = 32
 Dataset=TensorDataset(data_X, data_Y)
 trainloader = DataLoader(Dataset, batch_size=batch_size, shuffle=True)
 
-num_epochs=10
+num_epochs=50
 
 # Training starten
 for epoch in range(num_epochs):
     correct = 0
-    for i, data in enumerate(trainloader, 0):
+    total=0
+    for i, data in enumerate(trainloader):
         inputs, labels = data
         optimizer.zero_grad()
         outputs = net(inputs)
         loss = criterion(outputs, labels)
         loss.backward()
         optimizer.step()
-        correct += (outputs == labels).float().sum()
-        accuracy = 100 * correct / len(data_X)
-    print('Epoch {}/{}: Loss = {:.4f}, Accuracy = {:.4f}'.format(epoch+1, num_epochs, loss.item(), accuracy))
 
+
+        # Vorhersagen bestimmen
+        _, predicted = torch.max(outputs.data, 1)
+        total += labels.size(0)
+        correct += (predicted == labels).sum().item()
+
+    accuracy = 100 * correct / total
+    print('Epoch {}/{}: Loss = {:.4f}, Accuracy = {:.4f}'.format(epoch+1, num_epochs, loss.item(), accuracy))

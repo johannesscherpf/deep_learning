519961415f8029acfd7ef409263cfe4acb3606d6
hannes <johannespetersamuel.scherpf@student.hs-anhalt.de>
2025-10-20 17:17:01 +0200

    model py

--- src/model.py
+++ src/model.py
@@ -1,15 +1,20 @@
 import torch
 import numpy as np
+import os
 
 class Model:
     def __init__(self):
         # Gesamtes Modell laden
-        self.net = torch.load("solar.pth", map_location="cpu")
+        model_path = os.path.join(os.path.dirname(__file__), "solar.pth")
+        self.net = torch.load(model_path, map_location="cpu")
+        #self.net = torch.load("solar.pth", map_location="cpu")
         self.net.eval()
 
     def predict(self, x_test: np.ndarray) -> np.ndarray:
         # In Tensor konvertieren
-        inputs = torch.from_numpy(x_test.astype(np.float32))
+        inputs = (x_test - x_test.mean) / x_test.std
+        inputs = torch.from_numpy(inputs.astype(np.float32))
+
         with torch.no_grad():
             outputs = self.net(inputs)
             _, predicted = torch.max(outputs, dim=1)
